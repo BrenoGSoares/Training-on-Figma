29e9e137d3ee8556746a9b098c1fc2c467300b8f
Breno <breno.givas@icloud.com>
2024-01-26 20:18:24 -0300

    feat: modal login

--- crypto folder/js/script.js
+++ crypto folder/js/script.js
@@ -1,7 +1,9 @@
 const openMenu = document.getElementById('logo-mobile-open')
 const closeMenu = document.getElementById('logo-mobile-close')
+const loginBtn = document.getElementById('login')
 
 const modal = document.querySelector('.mobile-content')
+const modalLogin = document.querySelector('.modal-container')
 
 openMenu.addEventListener('click', () => {
   modal.style.display = 'flex'
@@ -9,4 +11,13 @@ openMenu.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
   modal.style.display = 'none'
+})
+
+loginBtn.addEventListener('click', () => {
+  modalLogin.classList.add('mostrar')
+  modalLogin.addEventListener('click', (e) => {
+    if (e.target.id == 'modal-log') {
+      modalLogin.classList.remove('mostrar')
+    }
+  })
 })
